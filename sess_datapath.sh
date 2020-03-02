5a63aeb41982ab1830e2dd5d68aded9b686eb507
James Chapman <jchapman@katalix.com>
2020-03-02 12:22:09 +0000

    tolerate kernels which lack sysctl net.ipv6.conf.all.keep_addr_on_down
    sess_datapath.sh sets net.ipv6.conf.all.keep_addr_on_down when it sets
    up its test net namespaces. This causes the script to fail on old
    kernels that lack the sysctl. But when using those old kernels, we
    don't need to set the sysctl anyway, so have the test script just skip
    setting the sysctl in that case.

--- sess_datapath.sh
+++ sess_datapath.sh
@@ -105,7 +105,7 @@ create_ns()
     ip -netns ${ns} -6 ro add unreachable default metric 8192
 
     ip netns exec ${ns} sysctl -qw net.ipv4.ip_forward=1
-    ip netns exec ${ns} sysctl -qw net.ipv6.conf.all.keep_addr_on_down=1
+    test -f /proc/sys/net/ipv6/conf/all/keep_addr_on_down && ip netns exec ${ns} sysctl -qw net.ipv6.conf.all.keep_addr_on_down=1
     ip netns exec ${ns} sysctl -qw net.ipv6.conf.all.forwarding=1
     ip netns exec ${ns} sysctl -qw net.ipv6.conf.default.forwarding=1
     ip netns exec ${ns} sysctl -qw net.ipv6.conf.default.accept_dad=0
