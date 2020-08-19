9f00d18c7427f80a6e818675c22380262a214537
Tom Parkin <tparkin@katalix.com>
2020-08-19 12:41:29 +0100

    util_ppp: tidy up ppp helper code
     * whitespace fixups (tab/space, indentation, extra blank lines)
     * drop unused include statements
     * convert pppd logging infrastructure to use util.h macros for unified
       logging from test applications

--- src/util_ppp.c
+++ src/util_ppp.c
@@ -70,178 +70,43 @@
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <stdarg.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/file.h>
-#include <sys/stat.h>
-#include <sys/utsname.h>
-#include <sys/sysmacros.h>
-
 #include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <syslog.h>
-#include <string.h>
-#include <time.h>
-#include <memory.h>
-#include <utmp.h>
-#include <mntent.h>
-#include <signal.h>
 #include <fcntl.h>
-#include <ctype.h>
-#include <termios.h>
 #include <unistd.h>
-
-#include <asm/types.h>
-#include <net/if.h>
-#include <net/if_arp.h>
-#include <net/route.h>
-#include <netinet/if_ether.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <sys/ioctl.h>
 
 #include <linux/ppp_defs.h>
 #include <linux/if_ppp.h>
 
+#include "util.h"
 #include "util_ppp.h"
-
-#ifndef MAXIFNAMELEN
-#define MAXIFNAMELEN 16
-#endif
-
-/* We can get an EIO error on an ioctl if the modem has hung up */
-#define ok_error(num) ((num)==EIO)
-
-static void die(int rc)
-{
-    exit(rc);
-}
-
-/*
- * logit - does the hard work for fatal et al.
- */
-static void
-logit(level, fmt, args)
-    int level;
-    char *fmt;
-    va_list args;
-{
-    vprintf(fmt, args);
-    printf("\n");
-}
-
-/*
- * fatal - log an error message and die horribly.
- */
-static void
-fatal(char *fmt, ...)
-{
-    va_list pvar;
-
-    va_start(pvar, fmt);
-
-    logit(LOG_ERR, fmt, pvar);
-    va_end(pvar);
-
-    die(1);                     /* as promised */
-}
-
-/*
- * error - log an error message.
- */
-static void
-error(char *fmt, ...)
-{
-    va_list pvar;
-
-    va_start(pvar, fmt);
-
-    logit(LOG_ERR, fmt, pvar);
-    va_end(pvar);
-}
-
-/*
- * warn - log a warning message.
- */
-static void
-warn(char *fmt, ...)
-{
-    va_list pvar;
-
-    va_start(pvar, fmt);
-
-    logit(LOG_WARNING, fmt, pvar);
-    va_end(pvar);
-}
-
-/*
- * notice - log a notice-level message.
- */
-__attribute__((unused)) static void
-notice(char *fmt, ...)
-{
-    va_list pvar;
-
-    va_start(pvar, fmt);
-
-    logit(LOG_NOTICE, fmt, pvar);
-    va_end(pvar);
-}
-
-/*
- * info - log an informational message.
- */
-__attribute__((unused)) static void
-info(char *fmt, ...)
-{
-    va_list pvar;
-
-    va_start(pvar, fmt);
-
-    logit(LOG_INFO, fmt, pvar);
-    va_end(pvar);
-}
-
-/*
- * dbglog - log a debug message.
- */
-static void
-dbglog(char *fmt, ...)
-{
-    va_list pvar;
-
-    va_start(pvar, fmt);
-
-    logit(LOG_DEBUG, fmt, pvar);
-    va_end(pvar);
-}
 
 /*
  * make_ppp_unit - make a new ppp unit.
  */
 static int make_ppp_unit()
 {
-        int x, flags;
-        int ifunit;
-        int fd;
+    int ifunit = -1;
+    int flags, fd;
 
-        fd = open("/dev/ppp", O_RDWR);
-        if (fd < 0)
-                fatal("Couldn't open /dev/ppp: %m");
-        flags = fcntl(fd, F_GETFL);
-        if (flags == -1
-            || fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1)
-                warn("Couldn't set /dev/ppp to nonblock: %m");
+    fd = open("/dev/ppp", O_RDWR);
+    if (fd < 0) {
+        err("Couldn't open /dev/ppp: %m\n");
+        return -errno;
+    }
 
-        ifunit = -1;
-        x = ioctl(fd, PPPIOCNEWUNIT, &ifunit);
-        if (x < 0)
-                error("Couldn't create new ppp unit: %m");
+    flags = fcntl(fd, F_GETFL);
+    if (flags == -1 || fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        err("Couldn't set /dev/ppp to nonblock: %m\n");
+        return -errno;
+    }
 
-        return (x < 0) ? x : ifunit;
+    if (ioctl(fd, PPPIOCNEWUNIT, &ifunit) < 0) {
+        err("Couldn't create new ppp unit: %m\n");
+        return -errno;
+    }
+
+    return ifunit;
 }
 
 /********************************************************************
@@ -250,51 +115,52 @@ static int make_ppp_unit()
  */
 int ppp_generic_establish_ppp(int fd, int *unit)
 {
-        int flags;
-        int ifunit;
-        int chindex = 0;
+    int flags, ifunit;
+    int chindex = 0;
 
-        /* Open an instance of /dev/ppp and connect the channel to it */
-        if (ioctl(fd, PPPIOCGCHAN, &chindex) == -1) {
-            error("Couldn't get channel number: %m");
-            goto err;
-        }
-        dbglog("using channel %d", chindex);
-        fd = open("/dev/ppp", O_RDWR);
-        if (fd < 0) {
-            error("Couldn't reopen /dev/ppp: %m");
-            goto err;
-        }
-        //(void) fcntl(fd, F_SETFD, FD_CLOEXEC);
-        if (ioctl(fd, PPPIOCATTCHAN, &chindex) < 0) {
-            error("Couldn't attach to channel %d: %m", chindex);
-            goto err_close;
-        }
-        flags = fcntl(fd, F_GETFL);
-        if (flags == -1 || fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1)
-            warn("Couldn't set /dev/ppp (channel) to nonblock: %m");
+    /* Open an instance of /dev/ppp and connect the channel to it */
+    if (ioctl(fd, PPPIOCGCHAN, &chindex) == -1) {
+        err("Couldn't get channel number: %m\n");
+        goto err;
+    }
+    dbg("using channel %d\n", chindex);
 
-        /*
-         * Create a new PPP unit.
-         */
-        ifunit = make_ppp_unit();
-        if (ifunit < 0)
-            goto err_close;
+    fd = open("/dev/ppp", O_RDWR);
+    if (fd < 0) {
+        err("Couldn't reopen /dev/ppp: %m\n");
+        goto err;
+    }
 
-        if (ioctl(fd, PPPIOCCONNECT, &ifunit) < 0) {
-            error("Couldn't attach to PPP unit %d: %m", ifunit);
-            goto err_close;
-        }
+    if (ioctl(fd, PPPIOCATTCHAN, &chindex) < 0) {
+        err("Couldn't attach to channel %d: %m", chindex);
+        goto err_close;
+    }
 
-        if (unit) {
-            *unit = ifunit;
-        }
+    flags = fcntl(fd, F_GETFL);
+    if (flags == -1 || fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1)
+        err("Couldn't set /dev/ppp (channel) to nonblock: %m");
+
+    /*
+     * Create a new PPP unit.
+     */
+    ifunit = make_ppp_unit();
+    if (ifunit < 0)
+        goto err_close;
+
+    if (ioctl(fd, PPPIOCCONNECT, &ifunit) < 0) {
+        err("Couldn't attach to PPP unit %d: %m", ifunit);
+        goto err_close;
+    }
+
+    if (unit) {
+        *unit = ifunit;
+    }
 
     return fd;
 
- err_close:
+err_close:
     close(fd);
- err:
+err:
     return -1;
 }
 
@@ -312,11 +178,14 @@ void ppp_output_packet(int fd, unsigned char *p, int len)
     len -= 2;
 
     if (write(fd, p, len) < 0) {
-        if (errno == EWOULDBLOCK || errno == EAGAIN || errno == ENOBUFS
-            || errno == ENXIO || errno == EIO || errno == EINTR)
-            warn("write: warning: %m (%d)", errno);
-        else
-            error("write: %m (%d)", errno);
+        if (!(errno == EWOULDBLOCK ||
+                errno == EAGAIN ||
+                errno == ENOBUFS ||
+                errno == ENXIO ||
+                errno == EIO ||
+                errno == EINTR)) {
+            err("write: %m (%d)\n", errno);
+        }
     }
 }
 
@@ -336,10 +205,10 @@ int ppp_read_packet(int fd, unsigned char *buf)
 
     nr = read(fd, buf, len);
     if (nr < 0 && errno != EWOULDBLOCK && errno != EAGAIN
-	&& errno != EIO && errno != EINTR)
-	error("read: %m");
+            && errno != EIO && errno != EINTR)
+        err("read: %m\n");
     if (nr < 0 && errno == ENXIO)
-	return 0;
+        return 0;
 
     return (nr > 0)? nr+2: nr;
 }
